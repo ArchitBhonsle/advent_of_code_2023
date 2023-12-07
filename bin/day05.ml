7a144b71aa1746b37385fe54934231d82d9a915c
Archit Bhonsle <abhonsle2000@gmail.com>
2023-12-07 00:26:29 -0500

    day five, part two

--- bin/day05.ml
+++ bin/day05.ml
@@ -1,46 +1,26 @@
 open! Core
 
 let input = In_channel.(input_all stdin)
-let maps_re = {|\w+\-to\-\w+\s+map\:\n((?:\d+\s*)+\n?)|} |> Re.Pcre.re |> Re.compile
 
-type map =
-  { src : int
-  ; dst : int
+type range =
+  { beg : int
   ; len : int
   }
 
-let parse_map_str map_str =
-  map_str
-  |> String.split_lines
-  |> List.map ~f:String.strip
-  |> List.map ~f:(String.split ~on:' ')
-  |> List.map ~f:(List.map ~f:String.strip)
-  |> List.map ~f:(List.map ~f:int_of_string)
-  |> List.map ~f:(fun l ->
-    { dst = List.nth_exn l 0; src = List.nth_exn l 1; len = List.nth_exn l 2 })
-;;
+type translation =
+  { dst : int
+  ; src : range
+  }
 
-let maps =
-  input
-  |> Re.all maps_re
-  |> List.map ~f:(fun m -> Re.Group.get m 1)
-  |> List.map ~f:String.strip
-  |> List.map ~f:parse_map_str
-  |> List.map ~f:(List.sort ~compare:(fun a b -> compare a.src b.src))
-;;
-
-let maps_to from maplist =
-  maplist
-  |> List.rev
-  |> List.find ~f:(fun m -> m.src <= from)
-  |> (fun m ->
-       match m with
-       | Some mi -> mi
-       | None -> { src = -1; dst = -1; len = 0 })
-  |> fun { src; dst; len } -> if src + len > from then from - src + dst else from
-;;
-
+(* read seeds *)
 let seeds_re = {|seeds:\s+((?:\d+\s+)*)|} |> Re.Pcre.re |> Re.compile
+
+let rec into_pairs l =
+  match l with
+  | [] -> []
+  | a :: b :: rest -> (a, b) :: into_pairs rest
+  | _ -> raise (Invalid_argument "the list had odd number of elements")
+;;
 
 let seeds =
   input
@@ -49,16 +29,66 @@ let seeds =
   |> String.strip
   |> String.split ~on:' '
   |> List.map ~f:int_of_string
+  |> into_pairs
+  |> List.map ~f:(fun p -> { beg = fst p; len = snd p })
 ;;
 
-let rec search map_list_list from =
-  match map_list_list with
-  | [] -> from
-  | map_list :: rest -> search rest (maps_to from map_list)
+(* read stages *)
+let stages_re = {|\w+\-to\-\w+\s+map\:\n((?:\d+\s*)+\n?)|} |> Re.Pcre.re |> Re.compile
+
+let parse_stages_str stages_str =
+  stages_str
+  |> String.split_lines
+  |> List.map ~f:String.strip
+  |> List.map ~f:(String.split ~on:' ')
+  |> List.map ~f:(List.map ~f:String.strip)
+  |> List.map ~f:(List.map ~f:int_of_string)
+  |> List.map ~f:(fun l ->
+    { dst = List.nth_exn l 0; src = { beg = List.nth_exn l 1; len = List.nth_exn l 2 } })
 ;;
 
-let minimum =
-  seeds |> List.map ~f:(search maps) |> List.fold ~init:Int.max_value ~f:Int.min
+let stages =
+  input
+  |> Re.all stages_re
+  |> List.map ~f:(fun m -> Re.Group.get m 1)
+  |> List.map ~f:String.strip
+  |> List.map ~f:parse_stages_str
 ;;
 
-let () = printf "%d\n" minimum
+(* break `this` range into at most 3 parts where none intersect `using`s endpoints *)
+let break_range this using =
+  let a_end = using.beg + using.len in
+  let b_end = this.beg + this.len in
+  [ Int.min using.beg this.beg, Int.min a_end this.beg
+  ; Int.max this.beg using.beg, Int.min a_end b_end
+  ; Int.max b_end using.beg, Int.max a_end b_end
+  ]
+  |> List.map ~f:(fun p -> { beg = fst p; len = snd p - fst p })
+  |> List.filter ~f:(fun r -> r.len > 0)
+;;
+
+(* translate `this` range using `translations`*)
+let translate_range translations this =
+  translations
+  |> List.find ~f:(fun t -> t.src.beg <= this.beg && this.beg <= t.src.beg + t.src.len)
+  |> function
+  | Some m -> { beg = this.beg - m.src.beg + m.dst; len = this.len }
+  | None -> this
+;;
+
+(* run the simulation *)
+let final =
+  stages
+  |> List.fold ~init:seeds ~f:(fun curr stage ->
+    stage
+    |> List.fold ~init:curr ~f:(fun c t ->
+      c |> List.map ~f:(break_range t.src) |> List.concat)
+    |> List.map ~f:(translate_range stage))
+;;
+
+let () =
+  final
+  |> List.map ~f:(fun r -> r.beg)
+  |> List.fold ~init:Int.max_value ~f:Int.min
+  |> printf "%d\n"
+;;
--- heresy/day05.py
+++ heresy/day05.py
@@ -1,60 +1,74 @@
 import sys
 import re
 
+# PARSING INPUT
+
 inp = sys.stdin.read()
 
 
-def parse_mapping(mapping: str) -> [(int, int, int)]:
-    return [tuple([int(x) for x in i.split()]) for i in mapping.split('\n')]
-
-
-mappings_strs = re.findall(
-    r"\w+\-to\-\w+\s+map\:\n((?:\d+\s*)+\n?)", inp)
-mappingss = [parse_mapping(mappings_str.strip())
-             for mappings_str in mappings_strs]
-
-
-def break_range(r: (int, int), m: (int, int, int)):
-    rb, rl = r
-    _, mb, ml = m
-
-    parts = [
-        (min(rb, mb), min(rb + rl, mb) - min(rb, mb)),
-        (max(rb, mb), min(rb + rl, mb + ml) - max(rb, mb)),
-        (max(rb, mb + ml), max(rb + rl, mb + ml) - max(rb, mb + ml))
+def parse_mapping(mapping: str) -> list[tuple[int, int, int]]:
+    return [
+        (vals[0], vals[1], vals[2])
+        for vals in [[int(x) for x in i.split()] for i in mapping.split("\n")]
     ]
-
-    return [(b, l) for b, l in parts if l > 0]
-
-
-def translate_range(rang: (int, int), mappings: [(int, int, int)]):
-    translate_with = [mapping for mapping in mappings
-                      if mapping[1] <= rang[0]
-                      and mapping[1] + mapping[2] >= rang[0] + rang[1]]
-    print(rang, translate_with)
-    if len(translate_with):
-        tw = translate_with[0]
-        return (rang[0] - tw[1] + tw[0], rang[1])
-    else:
-        return rang
 
 
 seeds_strs = re.findall(r"seeds:\s+((?:\d+\s+)*)", inp)[0].strip()
 seeds_nums = [int(x) for x in seeds_strs.split()]
-seeds = list(zip(seeds_nums[::2], seeds_nums[1::2]))
+seeds: list[tuple[int, int]] = list(zip(seeds_nums[::2], seeds_nums[1::2]))
+
+mappings_strs = re.findall(r"\w+\-to\-\w+\s+map\:\n((?:\d+\s*)+\n?)", inp)
+mappingss: list[list[tuple[int, int, int]]] = [
+    parse_mapping(mappings_str.strip()) for mappings_str in mappings_strs
+]
 
 
-# curr = seeds.copy()
-# for mappings in mappingss:
-#     new = set()
-#     for c in curr:
-#         for mapping in mappings:
-#             for br in break_range(c, mapping):
-#                 new.add(br)
-#     curr = list(new)
-#     print("bef", curr)
-#     print(mappings)
-#     curr = [translate_range(c, mappings) for c in curr]
-#     print("aft", curr)
-#
-# print(min(curr))
+def break_range(a: tuple[int, int], b: tuple[int, int]) -> list[tuple[int, int]]:
+    astart, alen = a
+    aend = astart + alen
+    bstart, blen = b
+    bend = bstart + blen
+
+    parts = [
+        (min(astart, bstart), min(aend, bstart)),
+        (max(bstart, astart), min(aend, bend)),
+        (max(bend, astart), max(aend, bend)),
+    ]
+    parts = [(start, end - start) for start, end in parts]
+
+    return [(start, length) for start, length in parts if length > 0]
+
+
+def translate_range(
+    r: tuple[int, int], mappings: list[tuple[int, int, int]]
+) -> tuple[int, int]:
+    rstart, rlen = r
+    res = None
+    for mapping in mappings:
+        _, mstart, mlen = mapping
+        if mstart <= rstart <= (mstart + mlen):
+            res = mapping
+            break
+
+    if res is None:
+        return r
+
+    mto, mstart, mlen = res
+    return (rstart - mstart + mto, rlen)
+
+
+source = seeds
+for mappings in mappingss:
+    for _, start, length in mappings:
+        new_source: list[tuple[int, int]] = list()
+        for seed in source:
+            new_source.extend(break_range(seed, (start, length)))
+        source = new_source
+
+    translated: list[tuple[int, int]] = []
+    for seed in source:
+        translated.append(translate_range(seed, mappings))
+
+    source = translated
+
+print(min(source)[0])
