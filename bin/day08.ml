03c41f67c90703886f30bd0fc0ddf9b4decc5cbb
Archit Bhonsle <abhonsle2000@gmail.com>
2023-12-09 00:42:01 -0500

    day eight, part two

--- bin/day08.ml
+++ bin/day08.ml
@@ -4,7 +4,7 @@ let input = In_channel.(input_all stdin)
 let directions_re = {|[RL]+|} |> Re.Pcre.re |> Re.compile
 let map_re = {|(?:(\w+) = \((\w+), (\w+)\))|} |> Re.Pcre.re |> Re.compile
 
-let directions =
+let given_directions =
   input
   |> Re.exec directions_re
   |> (fun g -> Re.Group.get g 0)
@@ -18,20 +18,34 @@ let map =
   |> Map.of_alist_exn (module String)
 ;;
 
-let move start =
-  directions
-  |> List.fold ~init:start ~f:(fun acc curr ->
-    (match curr with
-     | 'L' -> fst
-     | 'R' -> snd
-     | _ -> raise (Invalid_argument "Not possible"))
-      (Map.find_exn map acc))
+let directions = Sequence.cycle_list_exn given_directions
+
+let starts =
+  map
+  |> Map.keys
+  |> List.filter ~f:(fun s -> Char.equal (String.get s (String.length s - 1)) 'A')
 ;;
 
-let rec loop start i =
-  match start with
-  | "ZZZ" -> i
-  | _ -> loop (move start) (i + 1)
+let move dir now =
+  (match dir with
+   | 'L' -> fst
+   | 'R' -> snd
+   | _ -> raise (Invalid_argument "Not possible"))
+    (Map.find_exn map now)
 ;;
 
-let () = loop "AAA" 0 |> ( * ) (List.length directions) |> printf "%d\n"
+let rec loop dirs i start =
+  if Char.equal (String.get start (String.length start - 1)) 'Z'
+  then i
+  else (
+    let head = Sequence.hd_exn dirs in
+    let tail = Sequence.tl_eagerly_exn dirs in
+    loop tail (i + 1) (move head start))
+;;
+
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let lcm a b = a * b / gcd a b
+
+let () =
+  List.map starts ~f:(loop directions 0) |> List.fold ~init:1 ~f:lcm |> printf "%d\n"
+;;
