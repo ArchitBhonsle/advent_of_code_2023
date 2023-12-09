3193576d385121560b11638edb7abc19a622ea5d
Archit Bhonsle <abhonsle2000@gmail.com>
2023-12-09 11:07:09 -0500

    day nine, part one

---
+++ bin/day09.ml
@@ -0,0 +1,19 @@
+open! Core
+
+let input =
+  In_channel.(input_lines stdin)
+  |> List.map ~f:(fun line ->
+    line |> String.strip |> String.split ~on:' ' |> List.map ~f:int_of_string)
+;;
+
+let rec extrapolate nums =
+  if nums |> List.for_all ~f:(Int.equal 0)
+  then 0
+  else
+    List.last_exn nums
+    + extrapolate
+        (List.zip_exn (nums |> List.rev |> List.tl_exn |> List.rev) (nums |> List.tl_exn)
+         |> List.map ~f:(fun (a, b) -> b - a))
+;;
+
+let () = input |> List.map ~f:extrapolate |> List.fold ~init:0 ~f:( + ) |> printf "%d\n"
