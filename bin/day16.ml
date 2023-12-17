ec950cea5f42ead37112d697c8a5455a49f48743
Archit Bhonsle <abhonsle2000@gmail.com>
2023-12-17 12:21:48 -0500

    day sixteen, part one

--- bin/day16.ml
+++ bin/day16.ml
@@ -1,59 +1,105 @@
 open! Core
 
-let data = In_channel.(input_all stdin) |> String.strip
+let lines = In_channel.(input_lines stdin)
+let nrows = lines |> List.length
+let ncols = lines |> List.hd_exn |> String.length
 
-type operation =
-  | Rem
-  | Add of int
+type cell =
+  | Empty
+  | VerticalSplitter
+  | HorizontalSplitter
+  | LeadingReflector
+  | LaggingReflector
 
-type step =
-  { label : string
-  ; op : operation
-  }
-
-let steps =
-  data
-  |> String.split ~on:','
-  |> List.map ~f:(fun s ->
-    let split = String.split_on_chars s ~on:[ '-'; '=' ] in
-    if String.contains s '='
-    then { label = List.hd_exn split; op = Add (split |> List.last_exn |> int_of_string) }
-    else { label = List.hd_exn split; op = Rem })
-;;
-
-let hash_label label =
-  String.fold label ~init:0 ~f:(fun acc c -> Int.rem ((acc + Char.to_int c) * 17) 256)
-;;
-
-let boxes =
-  steps
-  |> List.fold
-       ~init:(List.init 256 ~f:(fun _ -> []))
-       ~f:(fun acc step ->
-         List.mapi acc ~f:(fun idx box ->
-           if idx = hash_label step.label
-           then (
-             match step.op with
-             | Rem ->
-               List.filter box ~f:(fun lens ->
-                 lens |> fst |> String.equal step.label |> not)
-             | Add focal_length ->
-               if List.count box ~f:(fun p -> p |> fst |> String.equal step.label) <> 0
-               then
-                 List.map box ~f:(fun p ->
-                   if p |> fst |> String.equal step.label then fst p, focal_length else p)
-               else (step.label, focal_length) :: box)
-           else box))
-;;
+let layout = Array.make_matrix ~dimx:nrows ~dimy:ncols Empty
 
 let () =
-  boxes
-  |> List.map ~f:List.rev
-  |> List.map ~f:(fun box ->
-    box
-    |> List.mapi ~f:(fun idx curr -> (idx + 1) * snd curr)
-    |> List.fold ~init:0 ~f:( + ))
-  |> List.mapi ~f:(fun idx power -> (idx + 1) * power)
-  |> List.fold ~init:0 ~f:( + )
+  lines
+  |> List.iteri ~f:(fun i row ->
+    row
+    |> String.iteri ~f:(fun j c ->
+      layout.(i).(j)
+        <- (match c with
+            | '.' -> Empty
+            | '|' -> VerticalSplitter
+            | '-' -> HorizontalSplitter
+            | '\\' -> LeadingReflector
+            | '/' -> LaggingReflector
+            | _ -> "unreachable" |> failwith |> raise)))
+;;
+
+type direction =
+  | Right
+  | Left
+  | Up
+  | Down
+[@@deriving compare, sexp, hash]
+
+type sim_call =
+  { i : int
+  ; j : int
+  ; dir : direction
+  }
+[@@deriving compare, sexp, hash]
+
+module Vis = struct
+  type t = sim_call
+
+  let compare = compare_sim_call
+  let sexp_of_t = sexp_of_sim_call
+  let hash = hash_sim_call
+end
+
+let visited = Hashtbl.create (module Vis)
+
+let rec sim i j dir =
+  if i < 0 || j < 0 || i >= nrows || j >= ncols || Hashtbl.mem visited { i; j; dir }
+  then ()
+  else (
+    Hashtbl.add_exn visited ~key:{ i; j; dir } ~data:();
+    let rec helper i j dir cel =
+      match cel with
+      | Empty ->
+        (match dir with
+         | Right -> sim i (j + 1) dir
+         | Left -> sim i (j - 1) dir
+         | Up -> sim (i - 1) j dir
+         | Down -> sim (i + 1) j dir)
+      | VerticalSplitter ->
+        (match dir with
+         | Right | Left ->
+           helper i j Up Empty;
+           helper i j Down Empty
+         | _ -> helper i j dir Empty)
+      | HorizontalSplitter ->
+        (match dir with
+         | Up | Down ->
+           helper i j Right Empty;
+           helper i j Left Empty
+         | _ -> helper i j dir Empty)
+      | LeadingReflector ->
+        (match dir with
+         | Right -> helper i j Down Empty
+         | Left -> helper i j Up Empty
+         | Up -> helper i j Left Empty
+         | Down -> helper i j Right Empty)
+      | LaggingReflector ->
+        (match dir with
+         | Right -> helper i j Up Empty
+         | Left -> helper i j Down Empty
+         | Up -> helper i j Right Empty
+         | Down -> helper i j Left Empty)
+    in
+    helper i j dir layout.(i).(j))
+;;
+
+let () = sim 0 0 Right
+let touched = Array.make_matrix ~dimx:nrows ~dimy:ncols 0
+let () = Hashtbl.iter_keys visited ~f:(fun k -> touched.(k.i).(k.j) <- 1)
+
+let () =
+  touched
+  |> Array.map ~f:(Array.fold ~init:0 ~f:( + ))
+  |> Array.fold ~init:0 ~f:( + )
   |> printf "%d\n"
 ;;
